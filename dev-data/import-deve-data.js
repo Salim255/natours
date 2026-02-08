6d3d3b43adb30335f38098a37815b7248df65706
Salim <doshka23@gmail.com>
2026-02-08 15:52:35 +0100

    built: import data and delete data

---
+++ dev-data/import-deve-data.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const dotenv = require('dotenv'); //we need this to connect our node app to the configue file
+dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
+const Tour = require('./../models/tour-model');
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+console.log( DB)
+mongoose
+  //.connect(process.env.DATABASE_LOCAL, {to connect to the local server
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful');
+  }); //this connect will return a promese
+
+
+// READ JSO file
+const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+
+// Import data into database
+const importData = async() => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// DELETE ALL DATA from COLLECTION
+const deleteData = async() => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted')
+    } catch (error) {
+        
+    }
+}
